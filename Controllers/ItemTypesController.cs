e9a916d4daba0565016c0e844120543115180c17
PanKunik <kunik12254@o2.pl>
2019-07-03 12:29:11 +0200

    New project files

---
+++ Controllers/ItemTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bikevision.Models;
+
+namespace bikevision.Controllers
+{
+    public class ItemTypesController : Controller
+    {
+        private BikeVisionDBEntities2 db = new BikeVisionDBEntities2();
+
+        // GET: ItemTypes
+        public ActionResult Index()
+        {
+            return View(db.ItemType.ToList());
+        }
+
+        // GET: ItemTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = db.ItemType.Find(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // GET: ItemTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ItemTypes/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idItemType,type")] ItemType itemType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ItemType.Add(itemType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemType);
+        }
+
+        // GET: ItemTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = db.ItemType.Find(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // POST: ItemTypes/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idItemType,type")] ItemType itemType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(itemType);
+        }
+
+        // GET: ItemTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = db.ItemType.Find(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // POST: ItemTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ItemType itemType = db.ItemType.Find(id);
+            db.ItemType.Remove(itemType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
