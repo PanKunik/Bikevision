4c21586486fa08afdd3788ef9e39706b822e4356
PanKunik <kunik12254@o2.pl>
2019-08-14 12:36:28 +0200

    Bootstrap validate

---
+++ Scripts/contact-form.validation.js
@@ -0,0 +1,7 @@
+﻿bootstrapValidate('#name', 'max: 30: Your name must not be longer than 30 characters', function (isValid) {
+    if (isValid) {
+        alert('Element is valid');
+    } else {
+        alert('Element is invalid');
+    }
+});
