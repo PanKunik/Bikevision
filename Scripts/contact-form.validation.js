09cbe890fd15efe5ab81988b1edb4ef7adfe381b
PanKunik <kunik12254@o2.pl>
2019-08-14 13:10:53 +0200

    Form validation .js

--- Scripts/contact-form.validation.js
+++ Scripts/contact-form.validation.js
@@ -1,2 +1,11 @@
-﻿bootstrapValidate('#name', 'max: 30: Imię musi być krótsze niż 30 znaków.|required: To pole jest wymagane.');
+﻿$(function () {
+    $('#submit').submit(function () {
+        if ($('.invalid-feedback').length > 0) {
+            alert('the form is valid');
+        }
+    });
+});
+
+bootstrapValidate('#name', 'max: 30: Imię musi być krótsze niż 30 znaków.|required: To pole jest wymagane.');
 bootstrapValidate('#email', 'email: Wpisz poprawny adres email.|required: Podaj adres email.');
+bootstrapValidate('#message', 'min: 10: Treść wiadomości musi zawierać minimum 10 znaków.');
