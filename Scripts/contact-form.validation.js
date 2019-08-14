027fea30f0a22cfc0c8501bd135c08eff0e69ab3
PanKunik <kunik12254@o2.pl>
2019-08-14 14:12:22 +0200

    New bootstrap-validate

--- Scripts/contact-form.validation.js
+++ Scripts/contact-form.validation.js
@@ -1,11 +1,37 @@
 ﻿$(function () {
-    $('#submit').on("click", function () {
+    $.validate({
+        lang: 'pt'
+    });
+});
+
+/*$(function () {
+    $('#submit').on("click", function (e) {
         if ($('.invalid-feedback').length > 0) {
-            alert('the form is valid');
+            alert('Niepoprawnie wypełniłeś pola');
+            e.preventDefault(e);
         }
+        if ($('.valid-feedback').length > 0) {
+            alert('Poprawnie wypełniłeś pola');
+            e.preventDefault(e);
+        }
+    });
+});
+
+$(function () {
+    $('#name').on("focusin", function (e) {
+        $('#message').val($('#name').val());
+        
+    });
+    $('#name').on("focusout", function (e) {
+        $('#message').val($('#name').val());
+
+    });
+    $('#name').on("change", function (e) {
+        $('#message').val($('#name').val());
+
     });
 });
 
 bootstrapValidate('#name', 'max: 30: Imię musi być krótsze niż 30 znaków.|required: To pole jest wymagane.');
 bootstrapValidate('#email', 'email: Wpisz poprawny adres email.|required: Podaj adres email.');
-bootstrapValidate('#message', 'min: 10: Treść wiadomości musi zawierać minimum 10 znaków.');
+bootstrapValidate('#message', 'min: 10: Treść wiadomości musi zawierać minimum 10 znaków.');*/
