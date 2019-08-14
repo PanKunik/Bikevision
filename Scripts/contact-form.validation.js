e27ade752c9796456c2fcdfc0de69d49bf68e4fe
PanKunik <kunik12254@o2.pl>
2019-08-14 12:51:13 +0200

    Fix

--- Scripts/contact-form.validation.js
+++ Scripts/contact-form.validation.js
@@ -1,2 +1,2 @@
-﻿bootstrapValidate('#name', 'max: 30: Imię musi być krótsze niż 30 znaków.');
-bootstrapValidate('#email', 'email: Wpisz poprawny adres email.');
+﻿bootstrapValidate('#name', 'max: 30: Imię musi być krótsze niż 30 znaków.|required: To pole jest wymagane.');
+bootstrapValidate('#email', 'email: Wpisz poprawny adres email.|required: Podaj adres email.');
