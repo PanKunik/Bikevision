330957bbc8ef990deaa4b52b1ff73aba445f638b
PanKunik <kunik12254@o2.pl>
2019-10-11 14:59:31 +0200

    Add

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bikevision.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Service()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            ViewBag.Message = "Your signup page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/SaleTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using bikevision.Models;
+
+namespace bikevision.Controllers
+{
+    public class SaleTypesController : Controller
+    {
+        private BikeVisionDBEntities2 db = new BikeVisionDBEntities2();
+
+        // GET: SaleTypes
+        public ActionResult Index()
+        {
+            return View(db.SaleType.ToList());
+        }
+
+        // GET: SaleTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SaleType saleType = db.SaleType.Find(id);
+            if (saleType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(saleType);
+        }
+
+        // GET: SaleTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SaleTypes/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idSaleType,type")] SaleType saleType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SaleType.Add(saleType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(saleType);
+        }
+
+        // GET: SaleTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SaleType saleType = db.SaleType.Find(id);
+            if (saleType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(saleType);
+        }
+
+        // POST: SaleTypes/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idSaleType,type")] SaleType saleType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(saleType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(saleType);
+        }
+
+        // GET: SaleTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SaleType saleType = db.SaleType.Find(id);
+            if (saleType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(saleType);
+        }
+
+        // POST: SaleTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SaleType saleType = db.SaleType.Find(id);
+            db.SaleType.Remove(saleType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
