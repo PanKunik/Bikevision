39f5ea0232006e82b140d3e3e241c17168a8fc19
PanKunik <kunik12254@o2.pl>
2019-10-29 14:07:28 +0100

    Side menu update
    -Change text on button

--- Scripts/Shop/hidden-filters.js
+++ Scripts/Shop/hidden-filters.js
@@ -11,5 +11,6 @@ function showFilters() {
     $(".left-column").toggleClass("show-menu");
     $("body").css("overflow-y", $(".left-column").hasClass("show-menu") ? "hidden": "visible");
     $(".floating-button").css("left", $(".left-column").hasClass("show-menu") ? "5px" : "calc(50% - 100px)");
+    $(".floating-button").html($(".left-column").hasClass("show-menu") ? "Zamknij filtry" : "<i class=\"fas fa-filter\"></i> Filtruj");
     $(".left-column").css("padding-bottom", $(".left-column").hasClass("show-menu") ? "75px" : "0");
 }
