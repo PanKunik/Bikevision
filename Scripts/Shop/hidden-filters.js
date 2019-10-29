47f9660abeba0c5e958f9e6d7467a137798e65c7
PanKunik <kunik12254@o2.pl>
2019-10-29 10:29:22 +0100

    ProductList Page Update
    -Finished shopping-cards
    -Finished side-menu

--- Scripts/Shop/hidden-filters.js
+++ Scripts/Shop/hidden-filters.js
@@ -1,3 +1,14 @@
-﻿function showFilters() {
-    //$(".cards-container").toggleClass("show");
+﻿window.addEventListener("load", function () {
+    $("body").toggleClass("hidden-x");
+});
+
+window.addEventListener("unload", function () {
+    $("body").toggleClass("hidden-x");
+});
+
+function showFilters() {
+    $(".overlay").toggleClass("show-overlay");
+    $(".left-column").toggleClass("show-menu");
+    $("body").css("overflow-y", $(".left-column").hasClass("show-menu") ? "hidden": "visible");
+    $(".floating-button").css("transform", $(".left-column").hasClass("show-menu") ? "translateX(-100px)" : "translateX(0)");
 }
