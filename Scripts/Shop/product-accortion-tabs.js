7170b9dc58538b9fc62900e5c65357eb9fdc6a74
PanKunik <kunik12254@o2.pl>
2019-11-01 21:14:19 +0100

    Product page layout update (Tabs to Accordion)
    -Need to improve colors selecting
    -Need to make gallery works
    
    -Need to style opinions

--- Scripts/Shop/product-accortion-tabs.js
+++ Scripts/Shop/product-accortion-tabs.js
@@ -1,6 +1,49 @@
-﻿$(document).ready(function () {
-    $("input[type='radio'").click(function (event) {
-        $(".product__single-tab.d-block").removeClass("d-block");
-        $(".product__single-tab[data-id='" + event.target.id + "']").addClass("d-block");// + "    " + event.target.id);
+﻿var lastWindowState = "";
+
+function TabsToAccordion() {
+    if (($(".product__menu-card").css("display") == "block") && (lastWindowState != "smallScreen")) {
+        lastWindowState = "smallScreen";
+        $(".product__menu").find(".nav-link").removeClass("active");
+        $(".tab-pane").removeClass("show");
+        $(".tab-pane").removeClass("active");
+    }
+
+    if (($(".product__menu-card").css("display") == "none") && (lastWindowState != "bigScreen")) {
+        lastWindowState = "bigScreen";
+        $(".tab-pane").removeClass("show");
+        $(".tab-pane").removeClass("active");
+
+        $(".tab-pane").first().addClass("show");
+        $(".tab-pane").first().addClass("active");
+
+        $(".rotate").removeClass("down");
+
+        $(".product__menu").find(".nav-link").first().addClass("active");
+    }
+}
+
+$(document).ready(function () {
+    if ($(".product__menu-card").css("display") == "block") {
+        lastWindowState = "smallScreen";
+    }
+
+    if ($(".product__menu-card").css("display") == "none") {
+        lastWindowState = "bigScreen";
+
+        $(".tab-pane").first().addClass("show");
+        $(".tab-pane").first().addClass("active");
+
+        $(".product__menu").find(".nav-link").first().addClass("active");
+    }
+
+    TabsToAccordion();
+
+    $(".product__menu-card").click(function () {
+        $(this).next().toggleClass("show").toggleClass("active");
+        $(this).children("i.rotate").toggleClass("down");
+    });
+
+    $(window).resize(function () {
+        TabsToAccordion();
     });
 });
