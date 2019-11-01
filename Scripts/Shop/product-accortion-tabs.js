1fdc9d1637d4ad174e388bb9fd89eaddc328db9e
PanKunik <kunik12254@o2.pl>
2019-11-01 17:38:21 +0100

    Product page layout
    -Need to improve Tab/Accordion on bottom
    -Need to improve colors selecting
    -Need to make gallery works

---
+++ Scripts/Shop/product-accortion-tabs.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $("input[type='radio'").click(function (event) {
+        $(".product__single-tab.d-block").removeClass("d-block");
+        $(".product__single-tab[data-id='" + event.target.id + "']").addClass("d-block");// + "    " + event.target.id);
+    });
+});
