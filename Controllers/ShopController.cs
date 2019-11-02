7b11d4ee05ee0fe84ec6e657a22a9f88a87ec566
PanKunik <kunik12254@o2.pl>
2019-11-02 13:20:33 +0100

    Read data from DB

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using bikevision.Models;
 
 namespace bikevision.Controllers
 {
     public class ShopController : Controller
     {
+        private bikewayDBEntities db = new bikewayDBEntities();
         // GET: Shop
         public ActionResult Index()
         {
-            return View();
+            var query = from items in db.Items
+                    join categories in db.Categories on items.Category_idCategory equals categories.idCategory
+                    //where categories.category1 == "Ramy rowerowe"
+                    select items;
+
+            return View(query);
         }
         public ActionResult Product()
         {
@@ -19,7 +26,11 @@ namespace bikevision.Controllers
         }
         public ActionResult ProductList()
         {
-            return View();
+            var query = from items in db.Items
+                        join categories in db.Categories on items.Category_idCategory equals categories.idCategory
+                        select items;
+
+            return View(query);
         }
     }
 }
