df1d304d9752c84baf7e5ce55c197e6a8d02f09e
PanKunik <kunik12254@o2.pl>
2019-11-03 15:24:51 +0100

    Registration update

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,29 +1,71 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using bikevision.Models;
-using System.Threading.Tasks;
 
 namespace bikevision.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+        // GET: /Account
         public ActionResult Index()
         {
             return View();
         }
-        // GET: Account
+
+        // GET: /Account/Login
         public ActionResult Login()
         {
             return View();
         }
-        // GET: Account
+
+        // GET: /Account/SignUp
         public ActionResult SignUp()
         {
             return View();
+        }
+
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                UserStore<ApplicationUser> Store = new UserStore<ApplicationUser>(new ApplicationDbContext());
+                ApplicationUserManager userManager = new ApplicationUserManager(Store);
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Account");
+                }
+                AddErrors(result);
+            }
+
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
         }
 
         public async Task<string> AddUser()
