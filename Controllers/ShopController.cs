b14942589ade6f72daf348e7463ca5a6b232e34f
PanKunik <kunik12254@o2.pl>
2019-11-09 14:57:47 +0100

    Search input for products

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -12,13 +12,17 @@ namespace bikevision.Controllers
     {
         private bikewayDBEntities db = new bikewayDBEntities();
         // GET: Shop
-        public ActionResult Index(string keywords)
+        public ActionResult Index(string Searching)
         {
+            if(Searching != null && Searching != "")
+            {
+                return RedirectToAction("ProductList", new { Searching = Searching });
+            }
             CarouselItemsViewModel itemList = new CarouselItemsViewModel();
             
             itemList.ItemsOnPromotion = db.Items.Where(i => i.discount > 0).Take(15).ToList();
             itemList.NewestItems = db.Items.OrderByDescending(i => i.idItem).Take(15).ToList();
-            itemList.PopularItems = db.Items.Where(i => i.name.Contains("asdasd")).ToList();
+            itemList.PopularItems = db.Items.Take(15).ToList();
 
             if (itemList != null)
             {
@@ -42,13 +46,21 @@ namespace bikevision.Controllers
 
             return View(item);
         }
-        public ActionResult ProductList()
+        public ActionResult ProductList(string Searching)
         {
-            var query = from items in db.Items
-                        join categories in db.Categories on items.Category_idCategory equals categories.idCategory
-                        select items;
+            List<Item> items;
 
-            return View(query);
+            if (Searching == null || Searching == "")
+            {
+                items = db.Items.ToList();
+            }
+            else
+            {
+                items = db.Items.Where(i => i.name.Contains(Searching)).ToList();
+                ViewBag.keyword = Searching;
+            }
+
+            return View(items);
         }
 
         public ActionResult Favorites()
