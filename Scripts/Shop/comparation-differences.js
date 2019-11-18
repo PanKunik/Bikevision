0786ab194ca277d32d4a0fa769d28349e6b85758
PanKunik <kunik12254@o2.pl>
2019-11-18 14:00:31 +0100

    Working comparation (with highlited differences)

---
+++ Scripts/Shop/comparation-differences.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    var tableTds = $("td[name^='comparation_']");
+        
+    tableTds.each(function () {
+        var elements = $("td[name*='" + $(this).attr('name') + "']");
+        var namedTds = elements.toArray();
+        
+        var lengths = namedTds.length;
+        
+        var same = true;
+
+        for (var i = 1; i < lengths; i++) {
+            if (namedTds[0].innerText != namedTds[i].innerText)
+                same = false;
+        }
+
+        if (!same)
+            elements.parent().addClass("table-info");
+    });
+
+});
