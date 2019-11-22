c0dfd25d6cab9d7ec4df1e75ed98ba52b46ee46e
PanKunik <kunik12254@o2.pl>
2019-11-22 12:15:44 +0100

    Comparation fix

--- Scripts/Shop/comparation-differences.js
+++ Scripts/Shop/comparation-differences.js
@@ -3,20 +3,21 @@
     var tableTds = $("td[name^='comparation_']");
         
     tableTds.each(function () {
-        var elements = $("td[name*='" + $(this).attr('name') + "']");
+        var elements = $("td[name='" + $(this).attr('name') + "']");
         var namedTds = elements.toArray();
         
         var lengths = namedTds.length;
         
         var same = true;
+        if (lengths > 1) {
+            for (var i = 1; i < lengths; i++) {
+                if (namedTds[0].innerText != namedTds[i].innerText)
+                    same = false;
+            }
 
-        for (var i = 1; i < lengths; i++) {
-            if (namedTds[0].innerText != namedTds[i].innerText)
-                same = false;
+            if (!same)
+                elements.parent().addClass("table-info");
         }
-
-        if (!same)
-            elements.parent().addClass("table-info");
     });
 
 });
